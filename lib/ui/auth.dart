dc3b9d0c1a4be54f8ad03bb2d025545f9a937913
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2025-03-28 18:34:03 -0700

    proper auth screen

--- lib/ui/auth.dart
+++ lib/ui/auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:ui';
 import '../api/api.dart';
 import 'mainview.dart';
 
@@ -47,7 +48,10 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _codeController = TextEditingController();
   String _errorMessage = '';
+  bool _is2FA = false;
+  String ticket = '';
 
   Future<void> _login() async {
     final username = _usernameController.text;
@@ -62,11 +66,8 @@ class _LoginPageState extends State<LoginPage> {
     } catch (e) {
       setState(() {
         if ('$e'.contains('mfa')) {
-          final ticket = '$e'.split(':')[2];
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => TwoFactorAuthPage(ticket: ticket)),
-          );
+          _is2FA = true;
+          ticket = '$e'.split(':')[2];
         } else {
           _errorMessage = 'Invalid login credentials: $e';
         }
@@ -74,56 +75,11 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 20),
-            if (_errorMessage.isNotEmpty)
-              Text(_errorMessage, style: const TextStyle(color: Colors.red)),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TwoFactorAuthPage extends StatefulWidget {
-  final String ticket;
-
-  const TwoFactorAuthPage({required this.ticket, super.key});
-
-  @override
-  _TwoFactorAuthPageState createState() => _TwoFactorAuthPageState();
-}
-
-class _TwoFactorAuthPageState extends State<TwoFactorAuthPage> {
-  final _codeController = TextEditingController();
-  String _errorMessage = '';
-
   Future<void> _submitCode() async {
     final code = _codeController.text;
 
     try {
-      await Api.loginCode(code, widget.ticket, false);
+      await Api.loginCode(code, ticket, false);
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const MainView()),
@@ -137,30 +93,123 @@ class _TwoFactorAuthPageState extends State<TwoFactorAuthPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Enter 2FA Code")),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _codeController,
-                decoration: const InputDecoration(labelText: '2FA Code'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submitCode,
-                child: const Text('Submit'),
-              ),
-              const SizedBox(height: 20),
-              if (_errorMessage.isNotEmpty)
-                Text(_errorMessage, style: const TextStyle(color: Colors.red)),
-            ],
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.primary,
+                Colors.black
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
         ),
-      ),
+        Center(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              width: 350,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Login",
+                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+                  ),
+                  const SizedBox(height: 16),
+                  if (!_is2FA) ...[
+                    TextField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.black
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.black
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _login,
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.black
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ] else ...[
+                    TextField(
+                      controller: _codeController,
+                      decoration: const InputDecoration(
+                        labelText: '2FA Code',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.black
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _submitCode,
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.black
+                      ),
+                      child: const Text(
+                        'Submit Code',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
+                  if (_errorMessage.isNotEmpty)
+                    const SizedBox(height: 20),
+                    Text(
+                      _errorMessage,
+                      style: const TextStyle(
+                        color: Colors.red,
+                        backgroundColor: Colors.black
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
